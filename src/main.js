6cabbffdb85e0025ee61c9912134bb202be7c938
Jaylene <85104946+Jaylene208@users.noreply.github.com>
2022-03-04 18:14:34 +0800

    配置

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
+// import VueI18n from 'vue-i18n'
+import ElementUI from 'element-ui';
+
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 
+// Vue.use(VueI18n)
+// Vue.config.lang = 'zh-cn'
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
